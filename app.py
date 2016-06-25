e4e06dcbbe07594f6ae4ba7c8ee2b60c21a5cc99
Vincent Wu <wuv21@uw.edu>
2016-06-24 23:15:12 -0700

    created redirect urls and sample clone creation

--- app.py
+++ app.py
@@ -1,45 +1,79 @@
-from flask import Flask, render_template, g
-import os
+from flask import Flask, render_template, g, request, redirect, url_for
 import sqlite3
+import os
 
 app = Flask(__name__)
-DATABASE = 'sql/hiv2_drug_assay(1).db'
-
-def get_db():
-	db = getattr(g, '_database', None)
-	if db is None:
-		db = g._database = sqlite3.connect(DATABASE)
-
-	return db
+DATABASE = os.path.join(os.path.curdir, 'sql', 'hiv2_drug_assay.db')
 
 
 def create_db():
-	with open('sql/create_tables.sql', 'r') as file:
-		queries = file.read()
+    with open(os.path.join(os.path.curdir, 'sql', 'create_tables.sql'), 'r') as file:
+        queries = file.read()
 
-		conn = sqlite3.connect(DATABASE)
-		cur = conn.cursor()
+        conn = sqlite3.connect(DATABASE)
+        cur = conn.cursor()
 
-		cur.executescript(queries)
-		conn.commit()
-		cur.close()
-		conn.close()
+        cur.executescript(queries)
+        conn.commit()
+        cur.close()
+        conn.close()
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+
+    return db
 
 
 def query_db(query, args=(), one=False):
-	cur = get_db().execute(query, args)
-	rv = cur.fetchAll()
-	cur.close()
+    cur = get_db().execute(query, args)
 
-	return (rv[0] if rv else None) if one else rv
+    rv = cur.fetchall()
+    cur.close()
+
+    return (rv[0] if rv else None) if one else rv
+
+
+def insert_db(query, args=()):
+    conn = get_db()
+    cur = conn.cursor()
+
+    cur.execute(query, args)
+    conn.commit()
+
+
+@app.route('/clear_db', methods=["POST"])
+def clear_db():
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute("DELETE FROM Clone")
+    conn.commit()
+
+    return redirect(url_for('index'))
 
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    test = query_db("SELECT * FROM Clone")
+    print(test)
+    return render_template('index.html', r=query_db("SELECT * FROM Clone"))
+
+
+@app.route('/add_clone', methods=["POST"])
+def add_clone():
+    clone = (request.form.get('clone_name'),
+             request.form.get('clone_aa'),
+             request.form.get('clone_date'))
+
+    insert_db("INSERT INTO Clone(name, aa_changes, purify_date) VALUES(?, ?, ?)", args=clone)
+
+    return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
-	app.debug = True
-	app.run()
+    create_db()
+    app.debug = True
+    app.run()
 
