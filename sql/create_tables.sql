5e95fa6b1d2a5d956b06c212e48f7ef36de309f5
Vincent Wu <wuv21@uw.edu>
2016-07-04 20:43:31 -0700

    fixed date handling between front and back end

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS Clone  (
 
 CREATE TABLE IF NOT EXISTS Virus_Stock (
   id INTEGER PRIMARY KEY,
-  harvest_date TEXT NOT NULL,
+  harvest_date DATE NOT NULL,
   clone INT NOT NULL,
   ffu_per_ml INTEGER NOT NULL,
   FOREIGN KEY (clone) REFERENCES Clone(id)
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS Drug (
 
 CREATE TABLE IF NOT EXISTS Plate_Reading (
   id INTEGER PRIMARY KEY,
-  read_date TEXT NOT NULL,
+  read_date DATE NOT NULL,
   letter CHARACTER(1) NOT NULL,
   q1 INTEGER,
   q2 INTEGER,
@@ -47,7 +47,7 @@ CREATE TABLE IF NOT EXISTS Quadrant (
   concentration_max DECIMAL(10, 10) NOT NULL,
   concentration_inc DECIMAL(10, 10) NOT NULL,
   num_controls INTEGER NOT NULL,
-  FOREIGN KEY (virus_stock) REFERENCES Virus_Stock(id)
+  FOREIGN KEY (virus_stock) REFERENCES Virus_Stock(id),
   FOREIGN KEY (drug) REFERENCES Drug(id)
 );
 
