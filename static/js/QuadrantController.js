65d218b525028983ce4e048c25e58b8ab5a3e6a3
Vincent Wu <wuv21@uw.edu>
2016-09-18 20:12:35 -0700

    modularized javascript file

---
+++ static/js/QuadrantController.js
@@ -0,0 +1,54 @@
+angular.module('bioApp').controller('QuadrantController', ['$scope', '$http', 'baseAddress', function($scope, $http, baseAddress) {
+    $scope.plate = {
+        name: "",
+        date: "",
+        letter: ""
+    };
+    $scope.quads = {};
+
+    // quadrant visibility
+    $scope.quadrantVisible = [true, false, false, false];
+
+    $scope.selectQuadrant = function(q) {
+        $scope.quadrantVisible = [false, false, false, false];
+        $scope.quadrantVisible[q] = true;
+    };
+
+    $scope.getAllData = function() {
+        $http.get(baseAddress + '/get_all_stocks')
+            .success(function(resp) {
+                $scope.stockClones = resp;
+        });
+
+        $http.get(baseAddress + '/get_all_clones')
+            .success(function(resp) {
+                $scope.clones = resp;
+            });
+
+        $http.get(baseAddress + '/get_all_drugs')
+            .success(function(resp) {
+                $scope.allDrugs = resp;
+            });
+    };
+
+    $scope.refreshData = function() {
+        $scope.getAllData();
+        $scope.showAlert('Virus stocks and drugs updated', warning=false);
+    };
+
+    $scope.getAllData();
+
+    $scope.submitPlate = function() {
+        $scope.plate.quads = $scope.quads;
+        console.log($scope.plate);
+
+        $http.post(baseAddress + '/create_plate', $scope.plate)
+            .success(function(resp) {
+                $scope.showAlert(resp.msg, warning=false);
+                window.location = resp.next_url;
+            })
+            .error(function(resp) {
+                $scope.showAlert(resp.msg, warning=true);
+            });
+    }
+}]);
