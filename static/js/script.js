26e284920d8957751bda1d34b001310a873f6ece
Vincent <wuv21@uw.edu>
2016-09-20 13:29:07 -0700

    minor wording changes in quadrant

--- static/js/script.js
+++ static/js/script.js
@@ -23,13 +23,13 @@ bioApp.filter('convertFromSqlDate', function() {
   };
 });
 
-bioApp.directive('validFile',function(){
+bioApp.directive('validFile',function() {
   return {
-    require:'ngModel',
-    link:function(scope,el,attrs,ngModel){
+    require: 'ngModel',
+    link: function(scope,el,attrs,ngModel){
       //change event is fired when file is selected
-      el.bind('change',function(){
-        scope.$apply(function(){
+      el.bind('change', function() {
+        scope.$apply(function() {
           ngModel.$setViewValue(el.val());
           ngModel.$render();
         });
