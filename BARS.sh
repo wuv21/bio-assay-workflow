e464a344c570c173f7068c0482804f3a5c3d1fdd
Vince Wu <wuv21@uw.edu>
2017-07-07 14:01:52 -0700

    fixed shell commands again

--- BARS.sh
+++ BARS.sh
@@ -1,12 +1,22 @@
 source venv/bin/activate
 nohup python bars.py &
 
-sleep 2
+echo "Loading BARS and opening up your browser..."
+sleep 1
 open http://localhost:8080
 
-echo "Please type '1' to quit BARS"
-select yn in "quit"; do
-    case $yn in \
-        quit ) export BARSPROCESS=$(ps -A | grep ' python bars.py$' | awk '{print $1}'); kill $BARSPROCESS; deactivate; break;;
+while true; do
+    printf "\n"
+    read -p "Do you wish to quit BARS? (y or n) - " yn
+    case $yn in
+        [Yy]* ) \
+            export BARSPROCESS=$(ps -A | grep ' python bars.py$' | awk '{print $1}'); \
+            kill $BARSPROCESS; \
+            deactivate; \
+            echo "Quitting BARS now. See you later!"
+            break;;
+
+        [Nn]* ) echo "Okay, BARS will stay open.";;
+        * ) echo "Please type 'y' or 'n'";;
     esac
 done
