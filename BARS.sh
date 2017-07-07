cd0594f4f62a6693a70e8ed5c6c430a1c7071d01
Vince Wu <wuv21@uw.edu>
2017-07-07 13:25:17 -0700

    fixed starter script

--- BARS.sh
+++ BARS.sh
@@ -4,7 +4,7 @@ nohup python bars.py &
 sleep 2
 open http://localhost:8080
 
-echo "Please type 'quit' to quit BARS"
+echo "Please type '1' to quit BARS"
 select yn in "quit"; do
     case $yn in \
         quit ) export BARSPROCESS=$(ps -A | grep ' python bars.py$' | awk '{print $1}'); kill $BARSPROCESS; deactivate; break;;
